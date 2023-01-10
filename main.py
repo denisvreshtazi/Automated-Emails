939064f1a68e4b80c1a440bad6054b4bc0ae7411
denisvreshtazi <denisvreshtazi@yahoo.com>
2023-01-10 16:48:36 +0100

    refactoring

--- main.py
+++ main.py
@@ -4,19 +4,24 @@ import pandas as pd
 from news import NewsFeed
 import time
 
+
+def send_mail():
+    email = yagmail.SMTP(user="Insert your email", password="Insert your google app password")
+    today = datetime.datetime.now().strftime("%Y-%m-%d")
+    # An alternative: today = date.today().strftime("%Y-%m-%d")
+    yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+    news_feed = NewsFeed(row['interest'], today, yesterday).get()
+    email.send(to=row['email'],
+               subject=f"Here are the news about {row['interest']}  for today!",
+               contents=f"Dear {row['name']} \n Check out the news about {row['interest']} : \n {news_feed}",
+               attachments="design.txt")
+
+
 while True:
     # To execute it at a certain time of the day
     if datetime.datetime.now().hour == 17 and datetime.datetime.now().minute == 33:
         df = pd.read_excel('people.xlsx')
 
         for index, row in df.iterrows():
-            email = yagmail.SMTP(user="Insert your email", password="Insert your google app password")
-            today = datetime.datetime.now().strftime("%Y-%m-%d")
-            # An alternative: today = date.today().strftime("%Y-%m-%d")
-            yesterday = (datetime.datetime.now() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
-            news_feed = NewsFeed(row['interest'], today, yesterday).get()
-            email.send(to=row['email'],
-                       subject=f"Here are the news about {row['interest']}  for today!",
-                       contents=f"Dear {row['name']} \n Check out the news about {row['interest']} : \n {news_feed}",
-                       attachments="design.txt")
+            send_mail()
     time.sleep(60)
